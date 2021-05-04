f0734cb642c656856652813909c6c7351c594d6c
ayush-singh-au13 <singhvlsiayush@gmail.com>
2021-05-04 15:23:17 +0530

    todoList_reactJS

---
+++ src/components/Todo.js
@@ -0,0 +1,72 @@
+import React from "react";
+import TodoList from './TodoList';
+import './Todo.css';
+
+class Todo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          todo: "",
+          todos: [],
+        };
+    }
+
+
+  submit = (e) => {
+    e.preventDefault();
+    // this.setState(previousState => {
+    //     return {
+    //   todos: [...previousState.todos, previousState.todo],
+    //   todo: ''
+    //     };
+    // });
+    this.setState({
+        todos: [
+            ...this.state.todos,
+            
+                this.state.todo
+            
+        ] ,
+        todo : ''
+    })
+  };
+
+  onChangeHandler = (e) => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+
+  render() {
+      
+    let todoLists = this.state.todos.map((v,i) => {
+        return (
+        <TodoList
+        key={i} 
+        value={v}/> 
+        )
+    });
+    return (
+      <>
+      <div className="list">
+      <h1>To Do Lists</h1>
+        <form onSubmit={this.submit}>
+          <input
+            type="text"
+            name="todo"
+            placeholder="Add todo items"
+            value={this.state.todo}
+            onChange={this.onChangeHandler}
+          />
+          <button className="btn" type="submit">Add</button>
+        </form>
+        <ul>{todoLists}</ul>
+        </div>
+    </>
+    );
+  }
+}
+
+export default Todo;
