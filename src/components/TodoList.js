911384bbe11048c0796bc0b95dfc95cf489f83ac
Ayush Singh <67584283+ayush-singh-au13@users.noreply.github.com>
2021-05-07 15:25:20 +0530

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './TodoList.css'
 export default function TodoList(props) {
-    const { value } = props;
+  // console.log(props);
+    const { value} = props;
     return (
       <div className="todolist">
         <li style={{ listStyle: "none", color: "#e1701a", fontSize:"1.25rem" }}>
